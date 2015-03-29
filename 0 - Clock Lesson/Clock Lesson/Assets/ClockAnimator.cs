ecfe0f7efecf6ca406865e922bf787cb39a96fe5
pixrael <pixrael777@gmail.com>
2015-03-28 23:53:43 -0430

    commit
    Adding a simple project about creat a clock in Unity

---
+++ 0 - Clock Lesson/Clock Lesson/Assets/ClockAnimator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	private const float 
+		hoursToDegrees = 360f / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60f;
+
+	public Transform hours, minutes, seconds;
+
+	public bool analog;
+
+
+	void Update(){
+
+
+
+		if (analog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			hours.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalHours * -hoursToDegrees);
+			minutes.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalSeconds * -secondsToDegrees);
+
+		
+		} else {
+
+			DateTime time = DateTime.Now;
+
+			hours.transform.localRotation = Quaternion.Euler (0f,0f,-hoursToDegrees*time.Hour);
+			minutes.transform.localRotation = Quaternion.Euler (0f,0f,-minutesToDegrees*time.Minute);
+			seconds.transform.localRotation = Quaternion.Euler (0f,0f,-secondsToDegrees*time.Second);
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+}
